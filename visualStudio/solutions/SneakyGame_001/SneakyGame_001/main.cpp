54de1087f047299381cd76dd0242a3bcc2870387
Ciaran Bent [K00221230] <k00221230@student.lit.ie>
2018-10-17 22:33:29 +0100

    Imported previous repo
    Previous commit count: 56

---
+++ visualStudio/solutions/SneakyGame_001/SneakyGame_001/main.cpp
@@ -0,0 +1,14 @@
+/**
+*	@authors		Ciaran Bent, Owen O'Dea, Rory Ryan
+*	@creationDate	2018/09/20
+*
+*/
+
+//#include "stdafx.h"
+#include <iostream>
+
+int main() { 
+	std::cout << "Hello, World!";
+	
+	return(0);
+}
