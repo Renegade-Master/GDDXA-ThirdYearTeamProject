e97c86fcccbad5fc203801f1075ac987a94f452d
Ciaran Bent [K00221230] <k00221230@student.lit.ie>
2018-12-10 17:00:32 +0000

    Updated Solution
    ### Since Last Commit
    ---
    ---
    #### Additions
    - Reference to a MiniMap View in Engine.h
    ---
    #### Changes
    - N/A
    ---
    #### Removed
    - References to Splitscreen in 'Engine.h' and 'Draw.cpp' as we will not be implementing splitscreen mechanics

--- vsSolutions/shadowOperative_001/shadowOperative_001/Draw.cpp
+++ vsSolutions/shadowOperative_001/shadowOperative_001/Draw.cpp
@@ -15,83 +15,27 @@ void Engine::draw() {
 	// Update the shader parameters
 	m_RippleShader.setUniform("uTime", m_GameTimeTotal.asSeconds());
 
-	if (!m_SplitScreen)	{
-		// Switch to background view
-		m_Window.setView(m_BGMainView);
-		// Draw the background
-		//m_Window.draw(m_BackgroundSprite);
+	// Switch to background view
+	m_Window.setView(m_BGMainView);
+	// Draw the background
+	//m_Window.draw(m_BackgroundSprite);
 
-		// Draw the background, complete with shader effect
-		m_Window.draw(m_BackgroundSprite, &m_RippleShader);
+	// Draw the background, complete with shader effect
+	m_Window.draw(m_BackgroundSprite, &m_RippleShader);
 
-		// Switch to m_MainView
-		m_Window.setView(m_MainView);
+	// Switch to m_MainView
+	m_Window.setView(m_MainView);
 
-		// Draw the Level
-		m_Window.draw(m_VALevel, &m_TextureTiles);
+	// Draw the Level
+	m_Window.draw(m_VALevel, &m_TextureTiles);
 
-		// Draw thomas
-		m_Window.draw(m_Thomas.getSprite());
+	// Draw thomas
+	m_Window.draw(m_Thomas.getSprite());
 
-		// Draw the particle system
-		if (m_PS.running())	{
-			m_Window.draw(m_PS);
-		}
+	// Draw the particle system
+	if (m_PS.running())	{
+		m_Window.draw(m_PS);
 	}
-	else {
-		// Split-screen view is active
-
-		// First draw Thomas' side of the screen
-
-		// Switch to background view
-		m_Window.setView(m_BGLeftView);
-		// Draw the background
-		//m_Window.draw(m_BackgroundSprite);
-
-		// Draw the background, complete with shader effect
-		m_Window.draw(m_BackgroundSprite, &m_RippleShader);
-
-		// Switch to m_LeftView
-		m_Window.setView(m_LeftView);
-
-		// Draw the Level
-		m_Window.draw(m_VALevel, &m_TextureTiles);
-
-		// Draw thomas
-		m_Window.draw(m_Thomas.getSprite());
-
-		// Draw the particle system
-		if (m_PS.running())	{
-			m_Window.draw(m_PS);
-		}
-
-		// Now draw Bob's side of the screen
-
-		// Switch to background view
-		m_Window.setView(m_BGRightView);
-		// Draw the background
-		//m_Window.draw(m_BackgroundSprite);
-
-		// Draw the background, complete with shader effect
-		m_Window.draw(m_BackgroundSprite, &m_RippleShader);
-
-		// Switch to m_RightView
-		m_Window.setView(m_RightView);
-
-		// Draw the Level
-		m_Window.draw(m_VALevel, &m_TextureTiles);
-
-		// Draw thomas
-		m_Window.draw(m_Thomas.getSprite());
-
-		// Draw the particle system
-		if (m_PS.running())	{
-			m_Window.draw(m_PS);
-		}
-
-	}
-
-
 
 	// Draw the HUD
 	// Switch to m_HudView
@@ -101,7 +45,6 @@ void Engine::draw() {
 	if (!m_Playing)	{
 		m_Window.draw(m_Hud.getMessage());
 	}
-
 
 	// Show everything we have just drawn
 	m_Window.display();
