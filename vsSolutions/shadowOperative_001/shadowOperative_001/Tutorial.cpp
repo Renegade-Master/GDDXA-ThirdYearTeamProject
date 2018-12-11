8a000354d03cf268da70ef42785e5291368ed9da
Ciaran Bent [K00221230] <k00221230@student.lit.ie>
2018-12-11 16:53:51 +0000

    Updated Solution [NON-COMPILING]
    ### Since Last Commit
    ---
    ---
    #### Additions
    - Jumping tutorial text file.
    - KVP structure for assigning Tutorial Text to a Map.
    ---
    #### Changes
    - N/A
    ---
    #### Removed
    - N/A

--- vsSolutions/shadowOperative_001/shadowOperative_001/Tutorial.cpp
+++ vsSolutions/shadowOperative_001/shadowOperative_001/Tutorial.cpp
@@ -2,11 +2,10 @@
 *	@author			Ciaran Bent [K00221230]
 *					Owen O'Dea	[K00218956]
 *					Rory Ryan	[]
-*	@creationDate	2018/11/01	YYYY/MM/DD
+*	@creationDate	2018/12/11	YYYY/MM/DD
 *	@description
 */
 
-#include <assert.h>
 #include "Tutorial.h"
 
 Tutorial* Tutorial::m_s_Instance = nullptr;
@@ -16,7 +15,40 @@ Tutorial::Tutorial() {
 	m_s_Instance = this;
 }
 
-std::string& Tutorial::GetTutorial(sf::Vector2i const& position) {
+std::string& Tutorial::GetTutorial(std::string const& filename) {
+	// Get a reference to m_Tutorials using m_S_Instance
+	std::map<std::string, std::string>& m = m_s_Instance->m_Tutorials;
+	// auto is the equivalent of map<string, string>
+
+	// Create an iterator to hold a key-value-pair (kvp)
+	// and search for the required kvp
+	// using the passed in file name
+	std::map<std::string, std::string>::iterator keyValuePair = m.find(filename);
+	// auto is equivalent of map<string, string>::iterator
+
+
+	// Did we find a match?
+	if (keyValuePair != m.end()) {
+		// Yes
+		// Return the string,
+		// the second part of the kvp, the string
+		return keyValuePair->second;
+	}
+	else {
+		// File name not found
+		// Create a new key value pair using the filename
+		auto& t_string = m[filename];
+		// Load the texture from file in the usual way
+		stream->open(filename);
+		std::string* temp = NULL;
+		stream->read(t_string, stream->getSize());
+		//t_string = m[*temp];
+		delete temp;
+
+		// Return the texture to the calling code
+		return(t_string);
+	}
+	
 	std::string temp = "NULL";
 	return *(&temp);
 }
--- vsSolutions/shadowOperative_001/shadowOperative_001/Tutorial.h
+++ vsSolutions/shadowOperative_001/shadowOperative_001/Tutorial.h
@@ -2,7 +2,7 @@
 *	@author			Ciaran Bent [K00221230]
 *					Owen O'Dea	[K00218956]
 *					Rory Ryan	[]
-*	@creationDate	2018/11/01	YYYY/MM/DD
+*	@creationDate	2018/12/11	YYYY/MM/DD
 *	@description
 */
 
@@ -10,14 +10,19 @@
 #ifndef TUTORIAL_H
 #define TUTORIAL_H
 
+#include <assert.h>
 #include <map>
 #include <SFML/Graphics.hpp>
+#include <SFML/System/FileInputStream.hpp>
 
 class Tutorial {
 private:
+	//  A pointer to the Tutorial to be displayed
+	static sf::FileInputStream* stream;
+
 	// A map container from the STL,
 	// that holds related pairs of String and Texture
-	std::map<std::string, sf::Texture> m_Textures;
+	std::map<std::string, std::string> m_Tutorials;
 
 	// A pointer of the same type as the class itself
 	// the one and only instance
@@ -25,6 +30,6 @@ private:
 
 public:
 	Tutorial();
-	static std::string& GetTutorial(sf::Vector2i const& position);
+	static std::string& GetTutorial(std::string const& filename);
 };
 #endif // !TUTORIAL_H
