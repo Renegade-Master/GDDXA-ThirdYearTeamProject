f05698c6d4a4829dcec79b37d1b6b2a0285d52c8
Ciaran Bent [K00221230] <k00221230@student.lit.ie>
2018-12-12 11:01:33 +0000

    Updated Solution
    ### Since Last Commit
    ---
    ---
    #### Additions
    - N/A
    ---
    #### Changes
    - Refactored EnemyGenerator.h and CreateEnemy.cpp
    ---
    #### Removed
    - N/A

--- vsSolutions/shadowOperative_001/shadowOperative_001/createEnemy.cpp
+++ vsSolutions/shadowOperative_001/shadowOperative_001/createEnemy.cpp
@@ -1,10 +1,19 @@
+/**
+*	@author			Ciaran Bent [K00221230]
+*					Owen O'Dea	[K00218956]
+*					Rory Ryan	[]
+*	@creationDate	2018/12/11	YYYY/MM/DD
+*	@description	Create a new Enemy Object
+*/
+
+
+#include <iostream>
 #include "Enemy.h"
 #include "EnemyGenerator.h"
-#include<iostream>
-Enemy* createEnemy(sf::Vector2i position,float Gravity)
-{
+
+Enemy* createEnemy(sf::Vector2i position,float Gravity) {
 	Enemy* enemy = new Enemy();
 	enemy->spawn(position, Gravity);
 	//std::cout << "\ncreated Enemy";
-	return enemy;
+	return(enemy);
 }
--- vsSolutions/shadowOperative_001/shadowOperative_001/enemyGenerator.h
+++ vsSolutions/shadowOperative_001/shadowOperative_001/enemyGenerator.h
@@ -1,8 +1,18 @@
+/**
+*	@author			Ciaran Bent [K00221230]
+*					Owen O'Dea	[K00218956]
+*					Rory Ryan	[]
+*	@creationDate	2018/12/11	YYYY/MM/DD
+*	@description	Create a new Enemy Object
+*/
+
 #pragma once
-//create a enemy object
 #ifndef ENEMYGENERATOR_H
 #define	ENEMYGENERATOR_H
+
 #include "Enemy.h"
+
 Enemy* createEnemy(sf::Vector2i position, float Gravity);
+
 #endif // !ENEMYGENERATOR_H
 
