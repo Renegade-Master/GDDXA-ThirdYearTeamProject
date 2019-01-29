be749ebc18238d9d7a688cc9a8654ef7d9ae6749
Ciaran Bent [K00221230] <k00221230@student.lit.ie>
2019-01-29 12:14:10 +0000

    Updated Solution
    ### Since Last Commit
    ---
    ---
    #### Additions
    - N/A
    ---
    #### Changes
    - N/A
    ---
    #### Removed
    - N/A

---
+++ vsSolutions/shadowOperative_001/shadowOperative_001/Constants.h
@@ -0,0 +1,11 @@
+/**
+*	@author			Ciaran Bent [K00221230]
+*					Owen O'Dea	[K00218956]
+*					Rory Ryan	[K00218864]
+*	@creationDate	2019/01/29	YYYY/MM/DD
+*	@description
+*/
+
+#pragma once
+
+constexpr int TILE_SIZE = 50;
